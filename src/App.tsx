6e68d933668213e14199e6ea413203df63ef8f99
agnibha-chatterjee <agni.bhaaa@gmail.com>
2024-02-17 01:39:01 -0800

    implmented a3 labs

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import React from "react";
-import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
-import Kanbas from "./Kanbas";
+import './App.css';
+import Labs from './Labs';
+import Kanbas from './Kanbas';
+import HelloWorld from './Labs/a3/HelloWorld';
+import { HashRouter } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router';
 
 function App() {
   return (
     <HashRouter>
       <div>
         <Routes>
-          <Route path="/" element={<Navigate to="Kanbas" />} />
+          <Route path="/" element={<Navigate to="/Labs" />} />
+          <Route path="/Labs/*" element={<Labs />} />
           <Route path="/Kanbas/*" element={<Kanbas />} />
+          <Route path="/hello" element={<HelloWorld />} />
         </Routes>
       </div>
     </HashRouter>
